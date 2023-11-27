33540af37059adc897bac88f55120f30fd95303d
tameem22 <tameemusa@gmail.com>
2023-11-26 20:35:28 -0600

    Added logout functionality

--- uih/website/views.py
+++ uih/website/views.py
@@ -11,10 +11,15 @@ def home(request):
 		user = authenticate(request, username=username, password=password)
 		if user is not None:
 			login(request, user)
-			messages.success(request, "You Have Been Logged In!")
+			messages.success(request, "You have heen hogged in!")
 			return redirect('home')
 		else:
 			messages.success(request, "There Was An Error Logging In, Please Try Again...")
 			return redirect('home')
 	else:
 		return render(request, 'home.html')
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out!")
+    return redirect('home')
