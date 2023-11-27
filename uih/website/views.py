116afea0d84914c9c196a06064e18e466203260d
tameem22 <tameemusa@gmail.com>
2023-11-26 19:15:47 -0600

    Added Nurse and Patient select on Login

--- uih/website/views.py
+++ uih/website/views.py
@@ -3,4 +3,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 def home(request):
+    # if request.method == 'POST':
+    #     username = request.post['username']
+    #     password = request.post['password']
+
     return render(request, 'home.html', {})
