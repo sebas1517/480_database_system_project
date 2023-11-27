88b5f651bc33fb48b667cfa497219e0bf4611c8e
tameem22 <tameemusa@gmail.com>
2023-11-26 20:29:43 -0600

    Added login errors

--- uih/website/views.py
+++ uih/website/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 def home(request):
-    # if request.method == 'POST':
-    #     username = request.post['username']
-    #     password = request.post['password']
+	# Check to see if logging in
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
 
-    return render(request, 'home.html', {})
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			messages.success(request, "You Have Been Logged In!")
+			return redirect('home')
+		else:
+			messages.success(request, "There Was An Error Logging In, Please Try Again...")
+			return redirect('home')
+	else:
+		return render(request, 'home.html')
